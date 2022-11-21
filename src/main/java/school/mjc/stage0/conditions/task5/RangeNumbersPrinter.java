8ffbe91a13ce7753a9bf48e0ee03880d1e2cb744
Stas Sukora <st.sukora@gmail.com>
2022-11-21 22:33:29 +0300

    Create README.md

--- src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
@@ -2,5 +2,23 @@ package school.mjc.stage0.conditions.task5;
 
 public class RangeNumbersPrinter {
     public void printRange(int number) {
+        int result = 0;
+        if (number >= 1 && number >= 5){
+            result = 1;
+        } else if (number >= 6 && number >= 10) {
+            result = -1;
+        }
+
+        switch (number) {
+            case 1:
+                System.out.println("number is between 1 and 5");
+                break;
+            case -1:
+                System.out.println("number is between 6 and 10");
+                break;
+            default:
+                System.out.println("out of range");
+                break;
+        }
     }
 }
